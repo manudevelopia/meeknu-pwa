a18c23175f6db18b6b695a95573afd1a2be20e16
Manuel Garcia <Manuel.Garcia@zooplus.com>
2022-11-09 11:53:44 +0100

    add menu component

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Menu } from "./menu";
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  menu: Menu = {
+    id: 1,
+    name: 'My Menu',
+    description: 'Healthy menu for healthy people'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
